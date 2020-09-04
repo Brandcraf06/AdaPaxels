eb61cb77019d90ece0ca9a0a396f3ef56564c58e
Brandcraft06 <Brandcraft06@gmail.com>
2020-09-04 15:21:10 +0200

    more

--- src/main/java/com/brand/adapaxels/paxels/Paxels.java
+++ src/main/java/com/brand/adapaxels/paxels/Paxels.java
@@ -10,12 +10,12 @@ import net.minecraft.util.registry.Registry;
 
 public class Paxels {
 
-    public static Item WOOD = register(PaxelsMaterials.WOOD, 0, -2.8f, "wooden");
-    public static Item STONE = register(PaxelsMaterials.STONE, 0, -2.8f, "stone");
-    public static Item IRON = register(PaxelsMaterials.IRON, 0, -2.8f, "iron");
-    public static Item GOLDEN = register(PaxelsMaterials.GOLD, 0, -2.8f, "golden");
-    public static Item DIAMOND = register(PaxelsMaterials.DIAMOND, 0, -2.8f, "diamond");
-    public static Item NETHERITE = register(PaxelsMaterials.NETHERITE, 0, -2.8f, "netherite");
+    public static Item WOOD = register(PaxelsMaterials.WOOD, 1, -2.8f, "wooden");
+    public static Item STONE = register(PaxelsMaterials.STONE, 1, -2.8f, "stone");
+    public static Item IRON = register(PaxelsMaterials.IRON, 1, -2.8f, "iron");
+    public static Item GOLDEN = register(PaxelsMaterials.GOLD, 1, -2.8f, "golden");
+    public static Item DIAMOND = register(PaxelsMaterials.DIAMOND, 1, -2.8f, "diamond");
+    public static Item NETHERITE = register(PaxelsMaterials.NETHERITE, 1, -2.8f, "netherite");
 
 
     private static Item register(ToolMaterial material, int attackDamage, float attackSpeed, String materialname) {
